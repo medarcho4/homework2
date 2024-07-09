1e5484b54ee92235109c508d0d41d8f7d973b609
medarcho4 <adammazhidov7@gmail.com>
2024-07-09 21:38:58 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+done_homeworks=12
+spent_hours=1.5
+name_of_course="Python"
+time_for_1lesson=spent_hours/done_homeworks
+print(f"Курс: {name_of_course}, всего задач: {done_homeworks}, затрачено время: {spent_hours}, среднее время выполнения {time_for_1lesson} часа")
